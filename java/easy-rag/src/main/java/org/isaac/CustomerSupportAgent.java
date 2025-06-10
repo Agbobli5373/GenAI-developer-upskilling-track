51fa5a010017843b2892fa986e7d7162cfcce0bf
Agbobli Isaac <agbobliisaackwadzo@gmail.com>
2025-06-10 15:36:17 +0000

    add

--- java/easy-rag/src/main/java/org/isaac/CustomerSupportAgent.java
+++ java/easy-rag/src/main/java/org/isaac/CustomerSupportAgent.java
@@ -1,4 +1,18 @@
-package org.isaac;
-
-public class CustomerSupportAgent {
-}
+//package org.isaac;
+//
+//
+//import dev.langchain4j.service.SystemMessage;
+//import io.quarkiverse.langchain4j.RegisterAiService;
+//import io.smallrye.mutiny.Multi;
+//
+//
+//@RegisterAiService
+//public interface CustomerSupportAgent {
+//
+//    @SystemMessage("""
+//            You are a customer support agent of a car rental company 'Miles of Smiles'.
+//            You are friendly, polite and concise.
+//            If the question is unrelated to car rental, you should politely redirect the customer to the right department.
+//            """)
+//    Multi<String> chat(String userMessage);
+//}
